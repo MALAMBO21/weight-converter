3a46743edae963c5ff49201aa1ec2d4ddf972985
MALAMBO21 <malambomainza80@gmail.com>
2026-03-23 15:57:04 +0200

    Options for conversion

--- BroCode/src/main/java/my/company/id/brocode/BroCode.java
+++ BroCode/src/main/java/my/company/id/brocode/BroCode.java
@@ -20,7 +20,29 @@ public class BroCode {
         System.out.println("1: Convert lbs to kgs");
          System.out.println("2: Convert kgs to lbs");
          
+         //Prompt user choice
          
+          System.out.println("Choose an option:");
+          choice = scanner.nextInt();
+          
+          //Option 1 Convert lbs to kgs
+          
+          if(choice == 1){
+               System.out.println("Enter your weight in lbs: :");
+               weight = scanner.nextDouble();
+               newWeight = weight * 0.7542;
+                System.out.println("Your weight is: " + newWeight);
+                
+                //Option 2 Convert kgs to lbs
+                
+                if(choice == 2){
+                     System.out.println("Enter your weight n kgs: ");
+                     weight = scanner.nextDouble();
+                     newWeight = weight / 0.7542;
+                     
+                }
+               
+          }
     
  scanner.close();
 }
