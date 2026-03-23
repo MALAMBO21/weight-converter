9e12575dc19f4162e369f35a257e1480ed6c82dc
MALAMBO21 <malambomainza80@gmail.com>
2026-03-23 17:03:12 +0200

    INITIAL COMMIT

---
+++ IceTask2/src/main/java/my/company/id/icetask2/IceTask2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package my.company.id.icetask2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class IceTask2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
