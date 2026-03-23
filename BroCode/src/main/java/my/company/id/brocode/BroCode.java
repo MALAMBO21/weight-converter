2bdbbee925b6aebcfbf9123986568d5e75673756
MALAMBO21 <malambomainza80@gmail.com>
2026-03-23 15:43:50 +0200

    Comp.Int

---
+++ BroCode/src/main/java/my/company/id/brocode/BroCode.java
@@ -0,0 +1,28 @@
+
+
+package my.company.id.brocode;
+
+import java.util.Scanner;
+
+public class BroCode {
+
+    public static void main(String[] args) {
+      
+       //Weight Converter Program
+       
+       Scanner scanner = new Scanner(System.in);
+       
+       double weight;
+       double newWeight;
+       int choice;
+       
+       System.out.println("Weight conversion program");
+        System.out.println("1: Convert lbs to kgs");
+         System.out.println("2: Convert kgs to lbs");
+         
+         
+    
+ scanner.close();
+}
+}
+        
