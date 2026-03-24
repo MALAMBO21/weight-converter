65124d52bf510c62efdfb95319346a0e95f319cf
MALAMBO21 <malambomainza80@gmail.com>
2026-03-24 10:59:46 +0200

    withdraw method added

--- bankprogram/src/main/java/my/company/id/bankprogram/Bankprogram.java
+++ bankprogram/src/main/java/my/company/id/bankprogram/Bankprogram.java
@@ -36,7 +36,7 @@ public class Bankprogram {
         switch(choice){
             case 1 -> System.out.println(balance);
             case 2 -> balance += deposit();
-            case 3 -> System.out.println("WITHDRAW");
+            case 3 -> balance -= withdraw(balance);
             case 4 -> isRunning = false; 
             default -> System.out.println("INVALID CHOICE");
         }
@@ -44,10 +44,7 @@ public class Bankprogram {
     }
   
        
-        
-        // DEPOSIT
-        
-        //WITHDRAW
+       
         
         //EXIT MESSAGE
        
@@ -77,6 +74,26 @@ public class Bankprogram {
           }
          
                
+    }
+    static double withdraw(double balance){
+        
+        double amount;
+        
+         System.out.print("Enter amount to be withdrawn: ");
+         amount = scanner.nextDouble();
+         
+         if(amount > balance){
+              System.out.println("INSUFFICIENT FUNDS");
+              return 0;
+         }
+         else if( amount < 0){
+              System.out.println("Amount can't be negative");
+              return 0;
+         }
+         else{
+             return amount;
+         }
+      
     }
 }
 
