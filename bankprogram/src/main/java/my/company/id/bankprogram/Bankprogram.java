1de62168faede1b0d2a3726d9d6681f5fd15d672
MALAMBO21 <malambomainza80@gmail.com>
2026-03-24 10:11:01 +0200

    initial commit

---
+++ bankprogram/src/main/java/my/company/id/bankprogram/Bankprogram.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package my.company.id.bankprogram;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Bankprogram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
