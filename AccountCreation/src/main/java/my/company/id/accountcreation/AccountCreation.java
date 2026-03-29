4d0dcfbb06443726082e8e4b26c0cafd8659e6e4
MALAMBO21 <malambomainza80@gmail.com>
2026-03-29 13:25:58 +0200

    method 1 update

--- AccountCreation/src/main/java/my/company/id/accountcreation/AccountCreation.java
+++ AccountCreation/src/main/java/my/company/id/accountcreation/AccountCreation.java
@@ -10,8 +10,9 @@ public class AccountCreation {
         
         Scanner scanner = new Scanner(System.in);
         
+       
         // Username Validation
-        
+         System.out.println("\n--- Login ---");
         System.out.println("Enter your username: ");
         String username = scanner.nextLine();
         if(username.matches(".*_.*") && username.length() <= 5){
@@ -28,9 +29,30 @@ public class AccountCreation {
         }else{
             System.out.println("Password is not correctly formatted, please ensure password contains atleast eight characters, a capital leter, a number, and a special character.");
         }
-        
-                           
+        // Phone Number Validation
+         System.out.println("Enter your phone number (+27...): ");
+         String phoneNumber = scanner.nextLine();
+         if(phoneNumber.length() <= 10){
+              System.out.println("Cellphone number Successfully added.");
+         }else{ 
+              System.out.println("Cellphone number incorrectly formatted or does not contain international code.");
+         }
+            if(){
+                 System.out.println("Welcome <user first name>,<user last name> it is great to see you again");
+            }else{
+                 System.out.println("Username or Passowrd incorrect, please try again.");
+            }               
         scanner.close();
     }
- 
+ public class Login{
+     
+//Method 1: Check Username
+     public boolean checkUserName(String username){
+         if(username == null || username.length() > 25){
+             return false;
+         }
+         return username.contains("_");
+     }
+     //Method 2 Check Password Complexity
+ }
    }
