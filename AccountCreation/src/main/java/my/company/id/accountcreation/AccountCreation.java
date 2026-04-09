760c3aac64a50a01ab483a0347d4674cdfbb61b6
MALAMBO21 <malambomainza80@gmail.com>
2026-04-09 13:39:44 +0200

    methods added

--- AccountCreation/src/main/java/my/company/id/accountcreation/AccountCreation.java
+++ AccountCreation/src/main/java/my/company/id/accountcreation/AccountCreation.java
@@ -20,8 +20,9 @@ public class AccountCreation {
         }else{
             System.out.println("Username not correctly formatted, please ensure username contains an underscore and no more than five characters in length.");
         }
-        // Password Validation
         
+        
+        // Password Validation
         System.out.println("Enter your password: ");
         String password = scanner.nextLine();
         if(password.length() >= 8 && password.matches("[A-Z]") && password.matches(".*@#!.*") && password.matches(".*[1-9].*")){
@@ -37,22 +38,63 @@ public class AccountCreation {
          }else{ 
               System.out.println("Cellphone number incorrectly formatted or does not contain international code.");
          }
-            if(){
-                 System.out.println("Welcome <user first name>,<user last name> it is great to see you again");
-            }else{
-                 System.out.println("Username or Passowrd incorrect, please try again.");
-            }               
+           
+         
+                      
         scanner.close();
     }
  public class Login{
      
 //Method 1: Check Username
      public boolean checkUserName(String username){
-         if(username == null || username.length() > 25){
+         if(username == null || username.length() > 5){
              return false;
          }
          return username.contains("_");
      }
      //Method 2 Check Password Complexity
+     public boolean checkPasswordComplexity(String password){
+         if(password == null || password.length() < 8){
+             return false;
+         }
+         return true;
+     }
+     //Method 3: Check Cell Phone number
+     public boolean checkCellPhoneNumber(String cellPhoneNumber){
+         if(cellPhoneNumber == null){
+             return false;
+         }
+         String regex = "\\+27\\d{9}$";
+         return cellPhoneNumber.matches(regex);
+     }
+     //Method 4: Register User
+     public String registerUser(String username, String password, String cellPhoneNumber){
+         if(!checkUserName(username)){
+             return "Username is incorrectly formatted, ysername must contain an underscore(_) and no more than 25 characters.";
+         }
+         if(!checkPasswordComplexity(password)){
+             return "Password does not meet the complexity requirements, password must contain atleast 8 characters long, contain a captuatl letter, a number, and a special character.";
+         }
+         if(!checkCellPhoneNumber(cellPhoneNumber)){
+             return "Cellphone is incorrectly formatted, it must in international format(e.g., +27XXXXXXXXX).";     
+         }
+         //if all checks pass
+         return "The two above conditions have been met, and the user has been registered successfully.";
+     }
+     //Method 5: Login User;
+     public boolean loginUser(String Username, String password){
+         String storedUsername = "main_21";
+         String storedPassword = "Password123!";
+         
+         return Username.equals(storedUsername) && password.equals(storedPassword);
+     }
+     //Method 6: Return Login Status
+     public String returnLoginStatus(boolean loginSuccessful){
+         if(loginSuccessful){
+             return "A successful login";
+         }else{
+             return "A failed login";
+         }
+     }
  }
    }
